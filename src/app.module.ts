6223ff00c8690341b3903a268f809514c5d6d258
Ved Parmar <parmarved1@gmail.com>
2026-09-15 17:49:08 +0530

    feat: register user and Prisma modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,17 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RedisModule } from './datasource/redis/redis.module';
+import { PrismaModule } from './datasource/postgres/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { UrlModule } from './modules/url/url.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      isGlobal: true, // makes ConfigService available everywhere, no need to re-import
-      envFilePath: '.env', // optional, this is the default anyway
+      isGlobal: true,
+      envFilePath: '.env',
     }),
+    PrismaModule,
     RedisModule,
     UrlModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
